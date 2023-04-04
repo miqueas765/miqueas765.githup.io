5f0a391a128df3560dc34524c21032cc54a6f906
miqueas765 <113945128+miqueas765@users.noreply.github.com>
2023-04-04 18:48:38 -0300

    Add files via upload

---
+++ fuction.js
@@ -0,0 +1,15 @@
+
+document.getElementById("btn").onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("contact").innerHTML = "3482501515";  
+   }
+    
+   
+   
+   
+
+
+
+
+
+
---
+++ fuction1.js
@@ -0,0 +1,4 @@
+document.getElementById("button").onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("gmail").innerHTML = "miqueduarte@gmail.com";  
+   }
